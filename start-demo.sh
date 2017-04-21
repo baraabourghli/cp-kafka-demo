219c11d4777286089fa9a18023bffe98c7969ec9
baraabourghli <baraa.bourghli@gmail.com>
2017-04-21 18:55:02 +0800

    UI config automation

--- start-demo.sh
+++ start-demo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+docker pull nicholedean/confluent:3.1.2
 
 docker-compose up -d
 
@@ -10,3 +11,24 @@ docker exec cpkafkademo_zookeeper_1 /opt/confluent/bin/kafka-topics --create --z
 docker exec cpkafkademo_zookeeper_1 /opt/confluent/bin/kafka-topics --create --zookeeper zookeeper:2181 --topic connect-status --replication-factor 1 --partitions 10
 
 docker-compose -f docker-compose.connect.yml up -d
+
+sleep 5
+
+cat << EOF > dev-myjsdb-adhoc_candidate_gift_voucher.json
+{
+  "name": "dev-myjsdb-adhoc_candidate_gift_voucher",
+  "config": {
+    "connector.class": "io.confluent.connect.jdbc.JdbcSourceConnector",
+    "tasks.max": "1",
+    "connection.url": "YourDBConnectionString",
+    "table.whitelist": "adhoc_candidate_gift_voucher",
+    "mode": "timestamp+incrementing",
+    "timestamp.column.name": "last_update",
+    "incrementing.column.name": "candidate_id",
+    "topic.prefix": "dev-myjsdb-"
+  }
+}
+EOF
+curl -X POST -H "Content-Type: application/json" -H "Accept: application/json" -d @dev-myjsdb-adhoc_candidate_gift_voucher.json http://localhost:8000/api/kafka-connect-1/connectors
+
+open http://localhost:8000/
